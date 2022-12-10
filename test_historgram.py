8e3a0cf96008fd0d7feecf10c2f0e3be7a561570
Davide Scintu <davidescintu195@gmail.com>
2022-12-10 16:13:22 +0100

    Test code to plot the histogram of an image modified using functions

--- test_historgram.py
+++ test_historgram.py
@@ -2,25 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 
-# read image
-im = cv2.imread('app/static/imagenet_subset/n01443537_goldfish.JPEG')
-# calculate mean value from RGB channels and flatten to 1D array
-vals = im.mean(axis=2).flatten()
-# calculate histogram
-counts, bins = np.histogram(vals, range(257))
-# plot histogram centered on values 0..255
-plt.bar(bins[:-1] - 0.5, counts, width=1, edgecolor='none')
-plt.xlim([-0.5, 255.5])
-plt.show()
 
-# plot histogram with 255 bins
-#b, bins, patches = plt.hist(vals, 255)
-#plt.xlim([0,255])
+def plot_histogram(image):
+    im = cv2.imread(image)
+    vals = im.mean(axis=2).flatten()
+    counts, bins = np.histogram(vals, range(257))
+    plt.bar(bins[:-1] - 0.5, counts, width=1, edgecolor='none')
+    plt.xlim([-0.5, 255.5])
+    plt.show()
+
+def plot_histogram_rgb(image):
+    im = cv2.imread(image)
+    color = ('b', 'g', 'r')
+    for i, col in enumerate(color):
+        histr = cv2.calcHist([im], [i], None, [256], [0, 256])
+        plt.plot(histr, color=col)
+        plt.xlim([0, 256])
+    plt.show()
 
 
-color = ('b','g','r')
-for i,col in enumerate(color):
-    histr = cv2.calcHist([im],[i],None,[256],[0,256])
-    plt.plot(histr,color = col)
-    plt.xlim([0,256])
-plt.show()
+img_one_ch = 'app/static/imagenet_subset/n01443537_goldfish.JPEG'
+plot_histogram(img_one_ch)
+
+img_rgb = 'app/static/imagenet_subset/n01443537_goldfish.JPEG'
+plot_histogram_rgb(img_rgb)
+
