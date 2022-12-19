5ad144ac0dc81044ddcfd79dff170f40e80da284
Davide Scintu <davidescintu195@gmail.com>
2022-12-19 15:12:49 +0100

    Modified form for the transformation using Float Field and NumberInput, putting default value to 1.0 to all enhancement's factors

--- app/forms/transformation_form.py
+++ app/forms/transformation_form.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, SelectField, FieldList, StringField, TextAreaField
+from wtforms import SubmitField, SelectField, FloatField
 from wtforms.validators import DataRequired
+from wtforms.widgets import NumberInput
 
 from app.utils.list_images import list_images
 from config import Configuration
@@ -9,14 +10,11 @@ conf = Configuration()
 
 
 class TransformationForm(FlaskForm):
-    transformation = SelectField('transformation', choices=conf.transformation_parameters, validators=[DataRequired()])
-    color = SelectField('color', choices=conf.color_input, validators=[DataRequired()])
-    brightness = SelectField('brightness', choices=conf.brightness_input, validators=[DataRequired()])
-    contrast = SelectField('contrast', choices=conf.contrast_input, validators=[DataRequired()])
-    sharpness = SelectField('sharpness', choices=conf.sharpness_input, validators=[DataRequired()])
-    # color_input = SubmitField('color_input')
-    # brightness_input = SubmitField('brightness_input')
-    # contrast_input = SubmitField('contrast_input')
-    # sharpness_input = SubmitField('sharpness_input')
+
+    color = FloatField(widget=NumberInput(step=0.1, min=-10.0, max=10.0), default=1.0, validators=[DataRequired()])
+    brightness = FloatField(widget=NumberInput(step=0.1, min=-10.0, max=10.0), default=1.0, validators=[DataRequired()])
+    contrast = FloatField(widget=NumberInput(step=0.1, min=-10.0, max=10.0), default=1.0, validators=[DataRequired()])
+    sharpness = FloatField(widget=NumberInput(step=0.1, min=-10.0, max=10.0), default=1.0, validators=[DataRequired()])
+
     image = SelectField('image', choices=list_images(), validators=[DataRequired()])
     submit = SubmitField('Submit')
