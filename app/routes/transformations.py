e9a119c80f420416f2757d7d0e88eee5e9c486c5
Davide Scintu <davidescintu195@gmail.com>
2022-12-19 15:14:08 +0100

    Modified transformations.py file to complete the new API functionality

--- app/routes/transformations.py
+++ app/routes/transformations.py
@@ -1,15 +1,8 @@
-import matplotlib.pyplot as plt
-import redis
 from flask import render_template
-from rq import Connection, Queue
-from rq.job import Job
-
 from app import app
-# from app.forms.classification_form import ClassificationForm
 from app.forms.transformation_form import TransformationForm
 from ml.classification_utils import transformation_image
 from config import Configuration
-
 
 config = Configuration()
 
@@ -26,14 +19,21 @@ def transformations():
         img_path = f'app/static/imagenet_subset/{image_id}'
         transformation_img_path = f'app/static/imagenet_transform/transform_{image_id}'
 
-        transformation_image(img_path, transformation_img_path, color_factor=1.5, brightness_factor=1.8,
-                             contrast_factor=1.9,
-                             sharpness_factor=1.7)  # test values
+        color_id = form.color.data
+        brightness_id = form.brightness.data
+        contrast_id = form.contrast.data
+        sharpness_id = form.sharpness.data
+
+        transformation_image(img_path, transformation_img_path, color_factor=color_id, brightness_factor=brightness_id,
+                             contrast_factor=contrast_id,
+                             sharpness_factor=sharpness_id)
+
+        # transformation_image(img_path, transformation_img_path, color_factor=1.5, brightness_factor=1.8,
+        #                     contrast_factor=1.9,
+        #                    sharpness_factor=1.7)  # test values
 
         return render_template("transformation_output.html", image_id=image_id)
 
     # otherwise, it is a get request and should return the
     # image and transformation selector
     return render_template('transformation_select.html', form=form)
-
-
